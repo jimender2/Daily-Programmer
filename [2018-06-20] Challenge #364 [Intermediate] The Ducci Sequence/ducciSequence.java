a57bcf9c268763d5d5f561bf6a4890ae14a32b08
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-06-30 14:32:53 -0400

    update too ducci

--- [2018-06-20] Challenge #364 [Intermediate] The Ducci Sequence/ducciSequence.java
+++ [2018-06-20] Challenge #364 [Intermediate] The Ducci Sequence/ducciSequence.java
@@ -4,12 +4,43 @@ import java.util.Scanner;
 public class ducciSequence {
 
 	public static void main(String[] args) throws IOException {
+		
+		//Read from file
 		File file = new File("ducci.txt");
 		Scanner scan = new Scanner(file);
+				
+		//declare a bunch of variables
+	    String wholeLine = "",
+	    	partLine = "",
+	    	split[];
+	    
+	    int number[],
+	    	difference,
+	    	length,
+	    	i = 1;
+	    
+	    
+		//Read each line
+		while (scan.hasNextLine()) {
+			
+			wholeLine = scan.nextLine();
+			//to get rid of the last ) and because arrays start at 0
+			length = (wholeLine.length() - 2);
+			
+			//to get rid of the ()
+			while (length >= i) {
+				partLine = partLine + wholeLine.charAt(i);
+				i++;
+			}
+			
+			split = partLine.split(", ");
+			
+			
+		}
 		
-		
-		
+		//close scanner
 		scan.close();
-	}
+		
+		}
 
 }
